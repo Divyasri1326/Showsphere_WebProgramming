6b6397ea74923ee877f43c9851604b88d4173a8a
Divyasri1326 <Divyaprcs@yahoo.com>
2025-06-29 18:59:58 +0800

    Add files via upload

---
+++ AccountController (In Progress).cs
@@ -0,0 +1,42 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Showsphere;
+using Showsphere.Models;
+using System.Linq;
+
+namespace Showsphere.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly ApplicationDbContext? _context;
+        
+
+        [HttpPost]
+
+        public IActionResult Login(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = users.FirstOrDefault(u => u.username == model.Username && u.password == model.Password);
+                if (user != null)
+                {
+                    ViewBag.Message = "Login Successful";
+                }
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Signup(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Sign up successful";
+                ModelState.Clear();
+            }
+            return View(model);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Showsphere.Models
+{
+    public class User
+    {
+        
+
+        [Required]
+        public required string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password
+        {
+            get; set;
+        }
+        
+
+    }
+    public class Signup
+    {
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        [MinLength(50)]
+        public required string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public required string RetypePassword { get; set; }
+
+        public bool AgreeToTerms { get; set; }
+    }
+}
